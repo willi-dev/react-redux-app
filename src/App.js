93f1d9e20d29c23a782407b04d45326f4eb878be
willi-dev <willi.ilmukomputer@gmail.com>
2020-07-20 08:25:19 +0700

    fix route, miss <Switch> tag

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import { AppRouter } from './router'
 import Menu from './_shared/Components/Menu'
 import './App.css'
 
-// import Error404 from './pages/Error/Error404'
+import Error404 from './pages/Error/Error404'
 
 function App() {
   return (
     <div className="App flex flex-wrap mb-4">
     <Menu router={AppRouter.filter( data => data.menu )}/>
-    {
-      AppRouter.map((route, index) => 
-        <Route path={route.path} component={route.component} exact={route.exact} key={index} />
-      )
-    }
-    {/* <Route path="*" component={Error404} /> */}
+    <Switch>
+      {
+        AppRouter.map((route, index) => 
+          <Route path={route.path} component={route.component} exact={route.exact} key={index} />
+        )
+      }
+      <Route component={Error404} />
+    </Switch>
     </div>
   );
 }
